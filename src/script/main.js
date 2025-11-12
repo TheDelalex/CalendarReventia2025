0a37ab6a1929ebf2c1f547c08df1912a6cdf894d
TheDelalex (Alexandre Delloue) <102219567+TheDelalex@users.noreply.github.com>
2025-11-12 13:48:12 +0100

    Changement d'image quand case cliqué

---
+++ src/script/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', function() {
+  // Récupérer l'objet d'état OU un objet vide
+  const ouverts = JSON.parse(localStorage.getItem('calendrierOuvert') || '{}');
+  document.querySelectorAll('.calendar-cell').forEach(cell => {
+    // On récupère le numéro de la case dans data-day
+    const day = cell.parentElement.href.match(/jour=(\d+)/)?.[1];
+
+    // Applique la classe "ouvert" à la case SEULEMENT si ce jour a été cliqué
+    if (ouverts[day]) cell.classList.add('ouvert');
+
+    // Gère le clic
+    cell.parentElement.addEventListener('click', function() {
+      // Marque le jour dans l'objet, puis sauvegarde
+      ouverts[day] = true;
+      localStorage.setItem('calendrierOuvert', JSON.stringify(ouverts));
+      cell.classList.add('ouvert');
+      // Le lien s'ouvre normalement
+    });
+  });
+});
